e4a32962284f1eb0bf47b695d10d64d7e3d5802d
Falcorfrenzie516 <84740784+Falcorfrenzie516@users.noreply.github.com>
2021-06-22 11:08:31 -0500

    version 2 update

--- main.js
+++ main.js
@@ -2,31 +2,36 @@
 // ## Welcome to Archnemesis, Inc. – the company that cares!
 // ### Requirements
 // 1. Write a unique morning greeting for each day of the week.
+let user = prompt("What is your login?");
 let today = new Date ();
 let dayOfWeek = today.getDay();
+let greeting;
+let longWeekend;
 
 if (dayOfWeek == 1) {
-document.write("Happy Monday - Let's kick the week off strong!");
+    greeting = ("Happy Monday " + user + " - Let's kick the week off strong!");
 }
 else if (dayOfWeek == 2) {
-document.write("It's Taco Tuesday!");
+    greeting = ("It's Taco Tuesday " + user + "!");
 }
 else if (dayOfWeek == 3) {
-document.write("Hump Day - half way done the week!");
+    greeting = ("Hump Day - half way done the week " + user + "!");
 }
-//else if (dayOfWeek == 4) {
-// document.write("Tee time Thursday!");
+//else if (dayOfWeek == 4) { 
 // Holiday check
-// if (dayOfWeek == 4){
+// if (dayOfWeek == longWeekend){
 //    
 //}
+//document.write("Tee time Thursday!");
 // }
 else if (dayOfWeek == 5) {
-document.write("We made it to end of the week!  Happy Friday!");
+    greeting = ("Congragulations " + user + ", you made it to end of the week!  Happy Friday!");
+}
+else{
+  greeting = ("Hey there " + user + ", hope you're having a great weekend!");
 }
 //
-//
-//
+document.write(greeting);
 //
 //
 // 2. If today is Thursday and Friday is a holiday, check if it is *also* a holiday. If so, begrudgingly demand that the employee enjoy their "long weekend."
